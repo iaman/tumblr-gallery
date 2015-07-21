67fbd4dd4be9024d5473f06f12c35bd893f2b96c
Ian O'Dea <ianodea@gmail.com>
2015-07-20 23:35:14 -0500

    Update messaging on form and make the viewer grab the viewer element

--- js/viewer.js
+++ js/viewer.js
@@ -1,6 +1,6 @@
 window.Viewer = {
   init: function () {
-    var form, input;
+    var form, input, viewer;
 
     form = tinyDOM.select('.viewer-form');
     if (form == null) return null;
@@ -8,22 +8,31 @@ window.Viewer = {
     input = tinyDOM.select('.viewer-form-input', form);
     if (input == null) return null;
 
+    viewer = tinyDOM.select('.viewer');
+    if (viewer == null) return null;
+
     this.form = form;
     this.input = input;
+    this.viewer = viewer;
 
     this.form.bind('submit', function (e) {
       e.preventDefault();
       form.setAttribute('disabled', true);
       tinyTumblr.get({
-        onError: function (data) {
-          form.find('p').html('Oops, that was not so good. Try another handle?');
+        onError: function () {
+          form.find('p').html('Oops, that was not so good. Try another username?');
           form.clearAttribute('disabled');
         },
         onSuccess: function (data) {
-          console.log(data);
+          viewer.style({display: 'block'});
+          Viewer.render(data.response.posts);
         },
         tumblr: input.getAttribute('value')
       });
     });
+  },
+
+  render: function (posts) {
+
   }
 }
