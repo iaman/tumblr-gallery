d9922ba9a710a2aba38b80a9a9da58412b7855f3
Ian O'Dea <ianodea@gmail.com>
2015-07-20 23:09:27 -0500

    Adding viewer form and allowing it to make requests

--- js/viewer.js
+++ js/viewer.js
@@ -2,10 +2,10 @@ window.Viewer = {
   init: function () {
     var form, input;
 
-    form = tinyDOM.select('.viewer-form')[0];
+    form = tinyDOM.select('.viewer-form');
     if (form == null) return null;
 
-    input = tinyDOM.select('.viewer-input');
+    input = tinyDOM.select('.viewer-form-input', form);
     if (input == null) return null;
 
     this.form = form;
@@ -13,6 +13,17 @@ window.Viewer = {
 
     this.form.bind('submit', function (e) {
       e.preventDefault();
+      form.setAttribute('disabled', true);
+      tinyTumblr.get({
+        onError: function (data) {
+          form.find('p').html('Oops, that was not so good. Try another handle?');
+          form.clearAttribute('disabled');
+        },
+        onSuccess: function (data) {
+          console.log(data);
+        },
+        tumblr: input.getAttribute('value')
+      });
     });
   }
 }
